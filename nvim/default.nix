7a656532c5662ffeda7b6b47f80e5985a67c0cdf
Adam Klepac <djklepy@gmail.com>
2025-07-21 22:18:37 +0200

    nvchad hotfix

--- nvim/default.nix
+++ nvim/default.nix
@@ -65,19 +65,19 @@
       tex-fmt
       alejandra
     ];
+
+    extraConfig = ''
+      config = function()
+        require("luasnip").config.set_config {
+          -- Autotriggered snippets
+          enable_autosnippets = true,
+
+          -- Tab to trigger visual selection
+          store_selection_keys = "<Tab>",
+        }
+      end,
+    '';
   };
-
-  extraConfig = ''
-    config = function()
-      require("luasnip").config.set_config {
-        -- Autotriggered snippets
-        enable_autosnippets = true,
-
-        -- Tab to trigger visual selection
-        store_selection_keys = "<Tab>",
-      }
-    end,
-  '';
 
   # Linking snippets folder
   xdg.configFile."nvim/snippets".source = ./snippets;
