8ef418d4e452f2e84b76391349d92a36f976263b
Adam Klepac <djklepy@gmail.com>
2025-07-24 19:54:34 +0200

    added hyprlock config

--- hyprland/hyprlock.nix
+++ hyprland/hyprlock.nix
@@ -12,16 +12,18 @@ in {
   programs.hyprlock = {
     enable = true;
     settings = {
-      background = {
-        path = "$HOME/Pictures/wallpaper.png";
-        blur_size = 4;
-        blur_passes = 3;
-        nose = 0.0117;
-        contrast = 1.3;
-        brightness = 0.8;
-        vibrancy = 0.21;
-        vibrancy_darkness = 0.0;
-      };
+      background = [
+        {
+          path = "$HOME/Pictures/wallpaper.png";
+          blur_size = 4;
+          blur_passes = 3;
+          nose = 0.0117;
+          contrast = 1.3;
+          brightness = 0.8;
+          vibrancy = 0.21;
+          vibrancy_darkness = 0.0;
+        }
+      ];
       label = [
         {
           text = "text = cmd[update:1000] echo \"<b><big> $(date +\"%H\") </big></b>";
@@ -50,6 +52,44 @@ in {
         {
           text = "cmd[update:18000000] echo \"<b><big> \"$(date +'%A')\" </big></b>";
           color = colors.blue;
+          font_size = 22;
+          font_family = "Hurmit Nerd Font 10";
+
+          position = "0, 30";
+          halign = "center";
+          valign = "center";
+        }
+        {
+          text = "cmd[update:18000000] echo \"<b> \"$(date +'%d %b')\" </b>";
+          color = colors.blue;
+          font_size = 18;
+          font_family = "Hurmit Nerd Font 10";
+
+          position = "0, 6";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+      input_field = [
+        {
+          size = "250, 50";
+          outline_thickness = 3;
+
+          dots_size = 0.26;
+          dots_spacing = 0.64;
+          dots_center = true;
+          dots_rounding = -1;
+
+          rounding = 22;
+          outer_color = colors.white0;
+          inner_color = colors.white0;
+          font_color = colors.cyan;
+          fade_on_empty = true;
+          placeholder_text = "<i>Password...</i>";
+
+          position = "0, 120";
+          halign = "center";
+          valign = "bottom";
         }
       ];
     };
