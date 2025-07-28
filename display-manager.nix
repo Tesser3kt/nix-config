db5ee659e3e78385fd51f39012a516dbf3d8ff0f
Adam Klepac <djklepy@gmail.com>
2025-07-28 09:45:53 +0200

    added theme to sddm

--- display-manager.nix
+++ display-manager.nix
@@ -7,4 +7,10 @@
   services.displayManager.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
+
+  # Add astronaut theme
+  environment.systemPackages = with pkgs; [
+    sddm-astronaut
+  ];
+  services.displayManager.sddm.theme = "sddm-astronaut-theme";
 }
