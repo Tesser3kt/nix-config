75d4bd33332dd3825c75dd90c63d498d9537a6f6
Adam Klepac <djklepy@gmail.com>
2025-08-14 14:42:02 +0200

    add wireguard tools

--- configuration.nix
+++ configuration.nix
@@ -169,6 +169,7 @@
     mcontrolcenter
     udiskie
     gnupg
+    wireguard-tools
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -218,13 +219,14 @@
   '';
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [51820 53317];
-  networking.firewall.allowedUDPPorts = [51820 53317];
+  networking.firewall.allowedTCPPorts = [53317];
+  networking.firewall.allowedUDPPorts = [53317];
+
+  # Allow wireguard through firewall
+  networking.firewall.checkReversePath = "loose";
+
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # Enable Mullvad VPN
-  services.mullvad-vpn.enable = true;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
