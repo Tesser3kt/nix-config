03ccb4a64c1ba7a860ae8520de67b77d73d68adf
Adam Klepac <djklepy@gmail.com>
2025-09-11 18:07:04 +0200

    add macaulay2

--- flake.nix
+++ flake.nix
@@ -56,8 +56,8 @@
 
         sourceRoot = "source/M2";
 
-        nativeBuildInputs = with prev; [cmake git pkg-config blas gdbm tbb libffi readline];
-        buildInputs = with prev; [gmp ncurses zlib boost];
+        nativeBuildInputs = with prev; [cmake git];
+        buildInputs = with prev; [gmp ncurses zlib boost readline pkg-config blas gdbm tbb libffi];
 
         meta = with prev.lib; {
           description = "Software system for research in algebraic geometry and commutative algebra";
