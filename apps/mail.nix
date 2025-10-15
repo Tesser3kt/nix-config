7e13216e858c34a246986f442c5997c3ff8ed77b
Adam Klepac <djklepy@gmail.com>
2025-10-15 20:39:54 +0200

    add goobook

--- apps/mail.nix
+++ apps/mail.nix
@@ -43,20 +43,31 @@
   };
 
   # Neomutt config for goobook
-  xdg.configFile."mutt/local.muttrc".text = ''
-    # example: Google Contacts via goobook
-    set query_command = "goobook query '%s'"
+  xdg.configFile."mutt/local-djklepy.muttrc".text = ''
+    set query_command = "goobook -c ~/.config/goobook-personal.rc query '%s'"
+    set query_format  = "%e\t%n"
+    bind editor <Tab> complete-query
+  '';
+  xdg.configFile."mutt/local-gevo.muttrc".text = ''
+    set query_command = "goobook -c ~/.config/goobook-work.rc query '%s'"
     set query_format  = "%e\t%n"
     bind editor <Tab> complete-query
   '';
 
   # Append a source line to mutt-wizard's muttrc, idempotently
-  home.activation.appendMuttLocal = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    file="$HOME/.config/mutt/muttrc"
+  home.activation.appendMuttLocal = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    file="$HOME/.config/mutt/accounts/djklepy@gmail.com.muttrc"
     mkdir -p "$(dirname "$file")"
     touch "$file"
-    if ! grep -q 'source "~/.config/mutt/local.muttrc"' "$file"; then
-      printf '\n# HM include: local overrides\nsource "~/.config/mutt/local.muttrc"\n' >> "$file"
+    if ! grep -q 'source "~/.config/mutt/local-djklepy.muttrc"' "$file"; then
+      printf '\n# HM include: local overrides\nsource "~/.config/mutt/local-djklepy.muttrc"\n' >> "$file"
+    fi
+
+    file="$HOME/.config/mutt/accounts/adam.klepac@gevo.cz.muttrc"
+    mkdir -p "$(dirname "$file")"
+    touch "$file"
+    if ! grep -q 'source "~/.config/mutt/local-gevo.muttrc"' "$file"; then
+      printf '\n# HM include: local overrides\nsource "~/.config/mutt/local-gevo.muttrc"\n' >> "$file"
     fi
   '';
 }
