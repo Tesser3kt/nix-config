61d3231499fc166b74106112bba300c57f9e88fc
Adam Klepac <djklepy@gmail.com>
2025-10-25 15:05:12 +0200

    fix vscode build

--- apps/vscode/default.nix
+++ apps/vscode/default.nix
@@ -11,7 +11,30 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode.overrideAttrs (old: {
-      buildInputs = (old.buildInputs or []) ++ [pkgs.cairo pkgs.pango];
+      buildInputs =
+        (old.buildInputs or [])
+        ++ [
+          pkgs.cairo
+          pkgs.pango
+          pkgs.glib
+          pkgs.gtk3
+          pkgs.gdk-pixbuf
+          pkgs.fontconfig
+          pkgs.freetype
+          pkgs.libX11
+          pkgs.libXext
+          pkgs.libXrender
+          pkgs.libXcursor
+          pkgs.libXdamage
+          pkgs.libXcomposite
+          pkgs.libXrandr
+          pkgs.libXi
+          pkgs.libXtst
+          pkgs.nss
+          pkgs.nspr
+          pkgs.expat
+          pkgs.dbus
+        ];
     });
     profiles.default = {
       enableUpdateCheck = true;
