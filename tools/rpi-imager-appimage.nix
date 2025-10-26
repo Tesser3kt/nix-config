bd14717e2a03675e78c53d1a2392a448dac49597
Adam Klepac <djklepy@gmail.com>
2025-10-26 12:54:56 +0100

    use rpi imager appimage instead

--- tools/rpi-imager-appimage.nix
+++ tools/rpi-imager-appimage.nix
@@ -6,7 +6,7 @@ appimageTools.wrapType2 {
   src = fetchurl {
     # Upstream AppImage URL for rpi-imager 1.9.6 (adjust version if you prefer)
     url = "https://downloads.raspberrypi.org/imager/imager_amd64.AppImage";
-    sha256 = lib.fakeSha256;  # run once to get the real hash (see below)
+    sha256 = "sha256-SG1pC75UqrJxkfE1+7/vwXzUjxmHtLn6AFK8/hpufUI=";  # run once to get the real hash (see below)
   };
   extraPkgs = pkgs: with pkgs; [ gsettings-desktop-schemas ];
   # Optional: set desktop file name/icon if needed, appimageTools handles most of it.
