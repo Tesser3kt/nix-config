799f7aadfc48e8dd68d8f573342104e0855d25f6
Adam Klepac <djklepy@gmail.com>
2025-11-26 16:47:14 +0100

    setup filen quickshell sync

--- apps/file-manager.nix
+++ apps/file-manager.nix
@@ -7,4 +7,20 @@
     xfce.thunar
     filen-cli
   ];
+
+  # Add Filen syncs
+  home.file.".config/filen-cli/syncPairs.json".text = ''
+    {
+      "syncPairs": [
+        {
+          "local": "${config.home.homeDirectory}/.config/quickshell",
+          "remote": "/quickshell",
+          "syncMode": "twoWay",
+          "alias": "quickshell-config",
+          "disableLocalTrash": true,
+          "excludeDotFiles": true
+        }
+      ]
+    }
+  '';
 }
