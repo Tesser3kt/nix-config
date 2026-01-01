bfa6d1826de40baef3896e4ce11c1026a3a91ff1
Adam Klepáč <djklepy@gmail.com>
2026-01-01 17:29:55 +0100

    make sage use python 3.12

--- apps/math.nix
+++ apps/math.nix
@@ -2,9 +2,25 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  pkgsForSage =
+    pkgs
+    // {
+      python3 = pkgs.python312;
+      python3Packages = pkgs.python312Packages;
+    };
+
+  mySageWithDoc = pkgs.sage.override {
+    pkgs = pkgsForSage;
+
+    withDoc = true;
+
+    # optional but often helpful to avoid long/brittle sage test runs during rebuilds
+    requireSageTests = false;
+  };
+in {
   programs.sagemath = {
     enable = true;
-    package = pkgs.sageWithDoc;
+    package = mySageWithDoc;
   };
 }
