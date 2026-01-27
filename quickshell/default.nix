522dba32b7f167c3da7d3b0b136552644e84a04f
Adam Klepac <djklepy@pm.me>
2026-01-27 11:55:40 +0100

    add quickshell config symlink for hot reload

--- quickshell/default.nix
+++ quickshell/default.nix
@@ -3,15 +3,16 @@
   pkgs,
   inputs,
   ...
-}: {
+}:
+let
+  quickshellConfig = "${config.home.homeDirectory}/nix-config/quickshell/config";
+in 
+{
   programs.quickshell = {
     enable = true;
     package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
     systemd.enable = true;
   };
 
-  home.file.".config/quickshell" = {
-    source = ./config;
-    recursive = true;
-  };
+  xdg.configFile."quickshell".source = config.lib.file.mkOutOfStoreSymlink quickshellConfig;
 }
