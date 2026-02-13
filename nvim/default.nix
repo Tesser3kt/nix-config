33cef2066b33f9f4a76445aef04673d5aa7a689d
Adam Klepáč <djklepy@gmail.com>
2026-02-13 22:31:31 +0100

    fix lazy updates for neovim

--- nvim/default.nix
+++ nvim/default.nix
@@ -1,10 +1,4 @@
-{
-  config,
-  pkgs,
-  lib,
-  inputs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ./linters.nix
     ./tools.nix
@@ -22,23 +16,24 @@
       docker-language-server
       emmet-language-server
       lua-language-server
-      (python3.withPackages (p: with p; [
-        python-lsp-server
-        python-lsp-black
-        pyls-isort
-        pyls-flake8
-        pylint
-        black
-        flake8
-        rope
-        pyflakes
-        mccabe
-        pycodestyle
-        pydocstyle
-        autopep8
-        yapf
-        isort
-      ]))
+      (python3.withPackages (p:
+        with p; [
+          python-lsp-server
+          python-lsp-black
+          pyls-isort
+          pyls-flake8
+          pylint
+          black
+          flake8
+          rope
+          pyflakes
+          mccabe
+          pycodestyle
+          pydocstyle
+          autopep8
+          yapf
+          isort
+        ]))
       sqls
       tailwindcss-language-server
       texlab
@@ -61,13 +56,12 @@
     ];
   };
 
-xdg.configFile."nvim".source = pkgs.fetchFromGitHub {
-  owner = "Tesser3kt";
-  repo  = "nvchad-starter";
+  xdg.configFile."nvim".source = pkgs.fetchFromGitHub {
+    owner = "Tesser3kt";
+    repo = "nvchad-starter";
 
-  rev = "12a9358a7cfdd3a124333d97682543d06972daab";
+    rev = "7c8f35650f2e8095cdd0943ab11d10c3e14c689c";
 
-  hash = "sha256-gpFWiqEezEZPQZRU6FDjXfAhSfWVQ4VgQOmPEV7h9PM=";
-};
-
+    hash = "sha256-O3NGFeCNtMus1r3yVDLg1i1/xyNjD5v/jjKQ2KyDJeU=";
+  };
 }
