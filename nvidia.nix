ef57eac6df1282f445eface60ba0a7d5f6feb104
Adam Klepac <djklepy@pm.me>
2026-02-23 09:57:09 +0100

    Revert "back to open nvidia"
    This reverts commit bb06a36aad4717ebffb779e2263173ebb7f8eda4.

--- nvidia.nix
+++ nvidia.nix
@@ -14,7 +14,7 @@
     modesetting.enable = true;
     powerManagement.enable = true;
     powerManagement.finegrained = false;
-    open = true;
+    open = false;
     nvidiaSettings = true;
   };
   services.xserver.videoDrivers = ["nvidia"];
