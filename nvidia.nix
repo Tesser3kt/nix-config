3b7e27be2f73c6ef0fd880fbf2662ba9fda82d42
Adam Klepac <djklepy@pm.me>
2026-02-23 09:57:11 +0100

    Revert "back to stable nviida"
    This reverts commit 7309be08d3d92d6c9161215102f3a3baf48951bb.

--- nvidia.nix
+++ nvidia.nix
@@ -16,6 +16,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
   services.xserver.videoDrivers = ["nvidia"];
   environment.sessionVariables = {
