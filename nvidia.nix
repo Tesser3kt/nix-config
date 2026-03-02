3804b06d00ca55c34225e12e2728cf22af6b78c3
Adam Klepac <djklepy@pm.me>
2026-03-02 19:11:28 +0100

    Revert "flake update"
    This reverts commit ac88f5bb113682023cb67746f5e3d4b8ba0132d1.

--- nvidia.nix
+++ nvidia.nix
@@ -1,35 +1,8 @@
 {
   config,
   pkgs,
-  lib,
   ...
 }: {
-  # Use an overlay to patch the nvidia beta driver
-  nixpkgs.overlays = [
-    (final: prev: {
-      linuxKernel = prev.linuxKernel // {
-        packages = lib.mapAttrs (version: kPkgs:
-          if kPkgs ? nvidiaPackages then
-            kPkgs // {
-              nvidiaPackages = kPkgs.nvidiaPackages // {
-                beta = kPkgs.nvidiaPackages.beta.overrideAttrs (oldAttrs: {
-                  postUnpack = (oldAttrs.postUnpack or "") + ''
-                    echo "=== Applying kernel 6.19 patch to NVIDIA open modules ==="
-                    if [ -d "source/kernel-open" ]; then
-                      cd source
-                      ${final.patch}/bin/patch -p1 < ${./kernel-6.19.patch}
-                      cd ..
-                    fi
-                  '';
-                });
-              };
-            }
-          else kPkgs
-        ) prev.linuxKernel.packages;
-      };
-    })
-  ];
-
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
@@ -37,7 +10,6 @@
       nvidia-vaapi-driver
     ];
   };
-
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
@@ -46,15 +18,12 @@
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
-
   services.xserver.videoDrivers = ["nvidia"];
-
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "nvidia";
     ELECTRON_OZONE_PLATFORM_HINT = "auto";
     NVD_BACKEND = "direct";
   };
-
   environment.systemPackages = with pkgs; [
     egl-wayland
   ];
