8d82dbf4e208e9710f994850604979d29fc8f451
Adam Klepac <djklepy@pm.me>
2026-03-03 18:15:57 +0100

    fix zoxide nushell completions

--- shell/nushell.nix
+++ shell/nushell.nix
@@ -32,21 +32,56 @@
       XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache";
     };
     extraConfig = ''
+      # Zoxide integration with custom completions
+      # Set up PWD change hook to add directories to zoxide
+      $env.config.hooks = ($env.config.hooks? | default {})
+      $env.config.hooks.env_change = ($env.config.hooks.env_change? | default {})
+      $env.config.hooks.env_change.PWD = ($env.config.hooks.env_change.PWD? | default [] | append {
+        code: {|_, dir| zoxide add -- $dir}
+      })
+
+      # Define custom completer for zoxide
+      def __zoxide_completer [context: string] {
+        zoxide query -l
+        | lines
+        | where {|x| $x != $env.PWD}
+        | each {|path|
+            {
+              value: ($path | path basename)
+              description: $path
+            }
+          }
+      }
+
+      # Define z command with completions
+      def --env "z" [
+        path?: string@__zoxide_completer  # path argument with custom completer
+      ] {
+        let target = if ($path | is-empty) {
+          "~"
+        } else if $path == "-" {
+          "-"
+        } else if ($path | path expand | path type) == "dir" {
+          $path
+        } else {
+          zoxide query --exclude $env.PWD -- $path | str trim -r -c "\n"
+        }
+        cd $target
+      }
+
+      # Define zi command for interactive selection
+      def --env "zi" [...rest: string] {
+        cd $"(zoxide query --interactive -- ...$rest | str trim -r -c "\n")"
+      }
+
       $env.config.completions.external = {
         enable: true
         max_results: 100
         completer: {|spans|
-          let cmd = $spans.0
-
-          # Use zoxide for 'z' command completions
-          if $cmd == 'z' {
-            ^zoxide query -l ...$spans | lines | where {|x| $x != $env.PWD}
-          } else {
-            # Fallback to carapace for other commands
-            carapace $cmd nushell ...$spans
-            | from json
-            | if ($in | default [] | where value =~ '^-.*ERR$' | is-empty) { $in } else { null }
-          }
+          # Fallback to carapace for external completions
+          carapace $spans.0 nushell ...$spans
+          | from json
+          | if ($in | default [] | where value =~ '^-.*ERR$' | is-empty) { $in } else { null }
         }
       }
     '';
@@ -57,6 +92,6 @@
   };
   programs.zoxide = {
     enable = true;
-    enableNushellIntegration = true;
+    enableNushellIntegration = false;  # We'll manually integrate with custom completions
   };
 }
