9d1d317d791d690fcbd939f8e386cfb6901b5ce9
Adam Klepac <djklepy@pm.me>
2026-05-16 16:27:02 +0200

    fixing negative ID workspace bug

--- hyprland/startup/raider.nix
+++ hyprland/startup/raider.nix
@@ -1,4 +1,5 @@
 [
   "mcontrolcenter &"
   "solaar -w hide &"
+  "sleep 0.5 && hyprctl --batch 'dispatch focusmonitor DP-2; dispatch workspace 1; dispatch focusmonitor eDP-1; dispatch workspace 6'"
 ]
