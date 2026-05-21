16a45f367c59740ede841e098623d5928b0e0c71
Adam Klepac <djklepy@pm.me>
2026-05-21 18:19:19 +0200

    disable vsync

--- hyprland_lua/config.nix
+++ hyprland_lua/config.nix
@@ -561,7 +561,7 @@ in {
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
         mouse_move_enables_dpms = true;
-        vrr = 1;
+        vrr = 0;
         focus_on_activate = false;
       };
 
