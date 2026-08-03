c4268f0cba7b1436cb022bf4a06c342e58c4d83d
Adam Klepac <djklepy@gmail.com>
2026-08-03 13:20:46 +0200

    add steam

--- configuration.nix
+++ configuration.nix
@@ -341,6 +341,14 @@
   # Allow wireguard through firewall
   networking.firewall.checkReversePath = "loose";
 
+  # Enable Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
