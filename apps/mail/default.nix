8ed37a4ac48c032673ac7ed8872b796ebd5852c7
Adam Klepac <djklepy@gmail.com>
2026-08-04 15:42:09 +0200

    fix goobook build

--- apps/mail/default.nix
+++ apps/mail/default.nix
@@ -1,27 +1,31 @@
 {
   config,
   pkgs,
+  pkgs-stable,
   ...
 }: {
   imports = [
     ./glow.nix
   ];
 
-  home.packages = with pkgs; [
-    neomutt
-    html2text
-    glow
-    lynx
-    notmuch
-    isync
-    openldap
-    abook
-    goobook
-    gcalcli
-    urlscan
-    pandoc
-    protonmail-bridge
-    protonmail-bridge-gui
-    pass
-  ];
+  home.packages = with pkgs;
+    [
+      neomutt
+      html2text
+      glow
+      lynx
+      notmuch
+      isync
+      openldap
+      abook
+      gcalcli
+      urlscan
+      pandoc
+      protonmail-bridge
+      protonmail-bridge-gui
+      pass
+    ]
+    ++ [
+      pkgs-stable.goobook
+    ];
 }
