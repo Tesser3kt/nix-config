7ea88055452212fbe075c176aa9fc6e4100816bf
Adam Klepac <djklepy@gmail.com>
2026-08-12 15:15:45 +0200

    fix flow theme

--- apps/mail/glow.nix
+++ apps/mail/glow.nix
@@ -1,3 +1,17 @@
 {config, ...}: {
-  xdg.configFile."glow".source = ./glow;
+  xdg.configFile."glow/glow.yml".text = ''
+    # style name or JSON path (default "auto")
+
+    style: "${config.home.homeDirectory}/.config/glow/catppuccin-mocha.json"
+    # show local files only; no network (TUI-mode only)
+    local: false
+    # mouse support (TUI-mode only)
+    mouse: false
+    # use pager to display markdown
+    pager: false
+    # word-wrap at width
+    width: 80
+  '';
+
+  xdg.configFile."glow/catppuccin-mocha.json".source = ./glow/catppuccin-mocha.json;
 }
