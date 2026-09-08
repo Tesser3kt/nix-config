7ca2cf97cae92f6fd9bd144b3c0fe53f14bd4d8e
Adam Klepac <djklepy@pm.me>
2026-09-08 20:34:20 +0200

    Revert "move goobook to unstable"
    This reverts commit 123359f8ae3a7892e0c87a9a770868aa70d19716.

--- apps/mail/default.nix
+++ apps/mail/default.nix
@@ -10,21 +10,24 @@
     ./mbsync.nix
   ];
 
-  home.packages = with pkgs; [
-    neomutt
-    html2text
-    glow
-    lynx
-    notmuch
-    isync
-    openldap
-    abook
-    gcalcli
-    urlscan
-    pandoc
-    protonmail-bridge
-    protonmail-bridge-gui
-    pass
-    goobook
-  ];
+  home.packages = with pkgs;
+    [
+      neomutt
+      html2text
+      glow
+      lynx
+      notmuch
+      isync
+      openldap
+      abook
+      gcalcli
+      urlscan
+      pandoc
+      protonmail-bridge
+      protonmail-bridge-gui
+      pass
+    ]
+    ++ [
+      pkgs-stable.goobook
+    ];
 }
