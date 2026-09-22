94fc31714b89cb05c3611d5eee27a5b4f941393e
Adam Klepac <djklepy@pm.me>
2026-09-22 15:14:29 +0200

    Revert "add excalidraw desktop app"
    This reverts commit e75a764aace3435078d50e04b20e1719dfb1d824.

--- apps/design.nix
+++ apps/design.nix
@@ -2,12 +2,9 @@
   config,
   pkgs,
   ...
-}: let
-  excalidraw-desktop = pkgs.callPackage ../pkgs/excalidraw-desktop.nix {};
-in {
+}: {
   home.packages = with pkgs; [
     gpick
     figma-linux
-    excalidraw-desktop
   ];
 }
